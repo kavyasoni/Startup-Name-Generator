569618ef1017fcdfbb76fc29d09e618795b0f514
Kavya Soni <kavyasoni88@gmail.com>
2020-04-12 21:31:51 +0530

    Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(SampleApp());
+}
+
+class SampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample HTTP App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sample App"),
+      ),
+      body: ListView.builder(
+        itemCount: widgets.length,
+        itemBuilder: (BuildContext context, int position) {
+          return getRow(position);
+        },
+      ),
+    );
+  }
+
+  Widget getRow(int i) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Text("Row ${widgets[i]["title"]}"),
+    );
+  }
+
+  Future<void> loadData() async {
+    String dataURL = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response;
+    response = await http.get(dataURL);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    setState(() {
+      widgets = json.decode(response.body);
+    });
+  }
+}
